5c3d4ec0294bd9d4185489cf98c470d843f6b0be
Vương Linh <Linh.VuongBao@Student.HTW-Berlin.de>
2025-03-16 20:51:44 +0700

    creating objects, putting them in a group and transforming the group

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene()
 
 /**
  * Objects
- */
+ 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
@@ -16,7 +16,7 @@ const mesh = new THREE.Mesh(geometry, material)
 mesh.position.x = 0.7
 mesh.position.y = -0.6
 mesh.position.z = 1
-*/
+
 scene.add(mesh)
 
 mesh.position.set(0.7, -0.6, 1)
@@ -29,6 +29,35 @@ mesh.rotation.z = 0
 
 //Scale
 mesh.scale.set(2, 0.5, 0.5)
+
+*/
+
+//Object group
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 //Axes Helper
 const axesHelper = new THREE.AxesHelper()
@@ -51,7 +80,7 @@ camera.position.y = 0
 camera.position.x = 0
 scene.add(camera)
 
-camera.lookAt(mesh.position)
+
   
 
 /**
